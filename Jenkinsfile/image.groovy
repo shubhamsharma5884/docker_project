00249482fe1e42d6b2654523752efa93cd9974a0
sharmashubham5884 <shubhamsharma5884@gmail.com>
2021-05-16 17:50:09 +0530

    adding file

--- Jenkinsfile/image.groovy
+++ Jenkinsfile/image.groovy
@@ -2,12 +2,14 @@ pipeline {
           environment {
     registry = "docker_hub_account/repository_name"
     registryCredential = 'dockerhub'
-  }  agent any  
-  stages {
-    stage('Building image') {
-      steps{
-        script {
-          docker.build registry + ":$BUILD_NUMBER"
+  }  
+  
+   agent any  
+     stages {
+        stage('Building image') {
+           steps{
+              script {
+                   docker.build registry + ":$BUILD_NUMBER"
         }
       }
     }
